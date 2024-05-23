34ee96b4c850d724d71fdea7c27ea117f9b6ec3d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-23 14:57:05 -0400

    Update tl_iso_product.php

--- system/modules/microcut_custom_product_management/dca/tl_iso_product.php
+++ system/modules/microcut_custom_product_management/dca/tl_iso_product.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    $GLOBALS['TL_DCA']['tl_iso_product']['list']['label']['label_callback'] = array
+    $GLOBALS['TL_DCA']['tl_iso_product']['list']['label']['label_callback'][0] = array
     (
         'IsotopeBcs\Backend\Product\LabelBcs' => 'generate'
     );
