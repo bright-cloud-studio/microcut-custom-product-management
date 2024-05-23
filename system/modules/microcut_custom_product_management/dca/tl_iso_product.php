3c83c2d4a9e83e2b92018951e79d6390c8c9370e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-23 15:06:12 -0400

    Update tl_iso_product.php

--- system/modules/microcut_custom_product_management/dca/tl_iso_product.php
+++ system/modules/microcut_custom_product_management/dca/tl_iso_product.php
@@ -5,4 +5,4 @@ use IsotopeBcs\Backend\Product\BcsLabel;
 $table = Isotope\Model\Product::getTable();
 
 $GLOBALS['TL_DCA'][$table]['list']['label']['label_callback.default'] = $GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'];
-$GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'] = [LabelBcs::class, 'generate'];
+$GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'] = [BcsLabel::class, 'generate'];
