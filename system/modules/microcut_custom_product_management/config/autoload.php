1bf90bd1a2833dfb10d72c1fe0fa8b65fd2021de
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-23 14:30:00 -0400

    Create autoload.php

---
+++ system/modules/microcut_custom_product_management/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+ 
+/**
+ * Zyppy Popup
+ *
+ * Copyright (C) 2018-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_popup
+ * @link       https://andrewstevens.consulting
+ */
+
+ 
+ 
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    'IsotopeBcs\Backend\Product\BcsLabel' 				=> 'system/modules/microcut_custom_product_management/library/IsotopeBcs/Backend/Product/BcsLabel.php',
+));
